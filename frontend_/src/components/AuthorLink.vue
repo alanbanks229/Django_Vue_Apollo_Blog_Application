96fa7efbd51a590bb400d452f0aee33daed3175d
Alan Banks <58575743+alanbanks229@users.noreply.github.com>
2021-07-20 17:02:57 -0400

    added new frontend folder directory

---
+++ frontend_/src/components/AuthorLink.vue
@@ -0,0 +1,32 @@
+
+
+<template>
+<!-- This component won't use GraphQL directly. -->
+<!-- instead, other components will pass in the autor information using the `author` prop -->
+  <router-link
+      :to="`/author/${author.user.username}`"
+  >{{ displayName }}</router-link>
+</template>
+
+<script>
+
+
+export default {
+  name: 'AuthorLink',
+  props: {
+    author: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    displayName () {
+      return (
+        this.author.user.firstName &&
+        this.author.user.lastName &&
+        `${this.author.user.firstName} ${this.author.user.lastName}`
+      ) || `${this.author.user.username}`
+    },
+  },
+}
+</script>
